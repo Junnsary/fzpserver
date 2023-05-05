28796dbf117a98bc48518b29e3b95a601fd02364
janzarr <hr.xiao@outlook.com>
2023-05-05 12:32:08 +0800

    tags小修改

--- controllers/tags.js
+++ controllers/tags.js
@@ -3,7 +3,7 @@ const tagsModel = require('../models/tags')
 //获取指定的type下的category的所有标签
 const category = async (req, res, next) => {
     res.header('content-type', 'application/json; charset=UTF-8')
-    const { type, category } = req.params
+    const { type, category } = req.query
     const tagResult = await tagsModel.findCategory(type, category)
     if (tagResult) {
         res.render('succ', {
