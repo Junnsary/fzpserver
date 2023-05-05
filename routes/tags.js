8aeb05d1eaeb163ec44d7abc10215170870cdb42
janzarr <hr.xiao@outlook.com>
2023-05-05 12:34:02 +0800

    tags

--- routes/tags.js
+++ routes/tags.js
@@ -3,7 +3,7 @@ var { category, knowledge, list, add, delTag } = require('../controllers/tags')
 var router = express.Router()
 
 //user - 获取标签的type、gategory
-router.get('/:type/:category', category)
+router.get('/typecategory', category)
 
 //user - 单独获取学习文章和学习视频的tag（为学习界面的 单独学习文章和视频列表的所有tagid）
 router.get('/knowledge', knowledge)
